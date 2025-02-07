c5bd623d52bf35410530670da2f11dd0161655f5
Harinath0608 <harinath192006@gmail.com>
2025-02-07 12:16:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { BrowserRouter, Routes, Route, Router } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <Navbar />
+
+    </div>
+      // <BrowserRouter>
+      // 
+      // <Router>
+      //   <Route> </Route>
+      // </Router>
+      // <Footer />
+      // </BrowserRouter>
+  )
+}
+
+export default App
